97376ab16bef822cdb8951ac305d467e0ec8faa9
Luigi Indaco <luigindaco@gmail.com>
2022-09-27 16:54:18 +0200

    consegna compito

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+//  function showBudget(){
+//      let budget = 10
+ 
+//      while (budget > 0 ){
+//         switch(budget){
+//             case budget < 5 :
+//                 alert('sei a metà budget')
+//                 break;
+//                 case budget < 2 :
+//                     alert('hai quasi finito il budget')
+//         }
+//              budget--;
+//          } }
+     
+function sottraiBudget(){
+let budget = 10
+while (budget > 0){
+    if (budget === 5){ 
+    alert('sei a metà budget')
+    } if  (budget === 2){
+        alert('hai quasi finito il budget')
+    }
+budget--
+console.log(budget)
+}
+}
+sottraiBudget()
+    
+
